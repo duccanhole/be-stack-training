d7024a4484bee6cd69f08addecb563f85457563e
duccanhole <duccanhole@gmail.com>
2023-11-30 21:38:57 +0700

    set up db

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,12 @@
-import { Module } from '@nestjs/common';
+import { Module } from '@nestjs/common'
+import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    MongooseModule.forRoot('mongodb+srv://duccanhole:<password>@cluster0.kz05mst.mongodb.net/')
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
