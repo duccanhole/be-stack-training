2c4f618d672e51713edcf8fb002bdb3a0c62fbf4
duccanhole <duccanhole@gmail.com>
2023-12-05 22:17:33 +0700

    add: get, get detail

--- src/module/todos/note.controller.ts
+++ src/module/todos/note.controller.ts
@@ -1,6 +1,10 @@
-import { Controller, Get, Post, Query, Res } from '@nestjs/common';
+import { Controller, Get, Param, Query, Res } from '@nestjs/common';
 import { NoteService } from './note.service';
 import { Response } from 'express';
+
+// class FormDataDTO {
+//   title: string
+// }
 
 @Controller('note')
 export class NoteController {
@@ -8,9 +12,31 @@ export class NoteController {
 
   @Get('/get')
   async getNotes(@Query() q, @Res() r: Response) {
+    const skip = parseInt(q.skip);
+    const limit = parseInt(q.limit);
     r.send({
-      results: await this.noteService.get(),
+      results: await this.noteService.get(skip, limit),
     });
+  }
+
+  @Get('/get/:noteId')
+  async getNotesDetail(@Param('noteId') id: string, @Res() r: Response) {
+    try {
+      const data = await this.noteService.getDetail(id);
+      if (!data) {
+        r.status(404).send({
+          message: 'Not Found',
+        });
+      } else {
+        r.send({
+          results: await this.noteService.getDetail(id),
+        });
+      }
+    } catch (error) {
+      r.send({
+        error,
+      });
+    }
   }
 
   @Get('/load-xls-data')
--- src/module/todos/note.service.ts
+++ src/module/todos/note.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Note, NoteDocument } from './note.schema';
 import { Model } from 'mongoose';
@@ -16,8 +16,22 @@ export class NoteService {
     @InjectModel(Note.name, 'db') private noteModel: Model<NoteDocument>,
   ) {}
 
-  async get() {
-    return await this.noteModel.find().limit(10).exec();
+  async get(skip: number = 0, limit: number = 10) {
+    return await this.noteModel.find().skip(skip).limit(limit).exec();
+  }
+
+  async getDetail(_id: string) {
+    const document = await this.noteModel.findById(_id);
+    if (!document) {
+      return null;
+    } else return document;
+  }
+
+  async create(title: string) {
+    await this.noteModel.create({
+      timestamp: new Date().toISOString(),
+      title,
+    });
   }
 
   async update(_id: string, title: string) {
