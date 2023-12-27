5c6bf2c54b539a41df54e4044bc264c102006234
duccanhole <duccanhole@gmail.com>
2023-12-27 22:29:11 +0700

    compose worker service

--- worker/app.module.ts
+++ worker/app.module.ts
@@ -1,5 +1,5 @@
 import { Module } from '@nestjs/common';
-import { MailerModule } from './mailer/mailer.module';
+import { MailerModule } from './src/mailer/mailer.module';
 // import { AppController } from './app.controller';
 // import { AppService } from './app.service';
 
